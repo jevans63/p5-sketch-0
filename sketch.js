9bfd9142027887caf8e53a71eeb7b1b00f19fe6d
jevans63 <jevans63@kent.edu>
2021-01-02 18:23:41 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,23 @@
 function setup()
 {
-    createCanvas(400, 400);
+    createCanvas(600, 600);
+    strokeWeight(.5);
+    frameRate(10);
+    background(0);
 }
   
 function draw() 
 {
-    background(220);
+    for (let i = 0; i < 1000; i+=10) 
+    {
+        let r = random(i+10);
+        stroke(i+5,100,r);
+        line(r, i, i, height);
+        stroke(0,10,r);
+        line(i, 0, 0, width);
+        line(r, r, i, r);
+        stroke(0,10,r);
+        line(r, i, i, height);
+    }
+
 }
